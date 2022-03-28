d6afbfa466e53e5e6f0f6bc3967f42f0685e3e75
onur1 <onur1@onurgunduz.com>
2022-03-28 15:02:23 +0200

    Update example

---
+++ example/README.md
@@ -0,0 +1,9 @@
+# stache example
+
+```
+npm run browserify
+npm run serve-frontend
+npm run serve-api
+```
+
+Navigate to [localhost:7890](http://localhost:7890/)
--- example/main.js
+++ example/main.js
@@ -1,12 +1,33 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const { createLayout, createElement } = require('@onur1/stache')
-
-const layout = createLayout(['div', {}, ['Hello, ', [3, 'name'], '!']])
-const data = { name: 'Onur' }
-const opts = { createElement: React.createElement }
-
-ReactDOM.render(
-  createElement(layout, data, opts),
-  document.getElementById('content')
-)
+const React = require("elm-ts/lib/React");
+const cmd = require("elm-ts/lib/Cmd");
+const { render } = require("react-dom");
+const { Layout, Template } = require("@onur1/stache/layout");
+const createComponent = require("@onur1/stache/react");
+const update = (_, model) => [model, cmd.none];
+const view = (layout) => (model) => (_) => layout(model);
+const init = [
+  {
+    items: [
+      { name: "Apple" },
+      { name: "Orange" },
+      { name: "Watermelon" },
+    ],
+  },
+  cmd.none,
+];
+fetch("http://localhost:7882/layout")
+  .then((response) => response.json())
+  .then((templates) => {
+    return React.run(
+      React.program(
+        init,
+        update,
+        view(
+          createComponent(
+            Layout(templates.map(([name, trees]) => Template(name, trees)))
+          )
+        )
+      ),
+      (dom) => render(dom, document.getElementById("content"))
+    );
+  });
