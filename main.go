a4238f1f20dd48e08be92295cc770f839b6950d9
onur1 <onur1@onurgunduz.com>
2022-04-08 20:44:22 +0200

    Allow passing glob as argument to CLI

--- main.go
+++ main.go
@@ -6,19 +6,38 @@ import (
 	"os"
 
 	"github.com/onur1/stache/parser"
+	"github.com/onur1/stache/template"
 )
 
 func main() {
-	if err := parser.Parse(os.Stdin, func(tree *parser.TagNode) bool {
-		b, err := json.Marshal(tree.Serialize())
+	if len(os.Args) > 1 {
+		tpls, err := template.ParseGlob(os.Args[1])
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println(string(b))
-		return true
-	}); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		data := make([]interface{}, len(tpls))
+		for i, v := range tpls {
+			data[i] = v.Serialize()
+		}
+		body, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(string(body))
+	} else {
+		if err := parser.Parse(os.Stdin, func(tree *parser.TagNode) bool {
+			b, err := json.Marshal(tree.Serialize())
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Println(string(b))
+			return true
+		}); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
