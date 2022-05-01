082ce223f24fb82e3b6edc06d3255b015f7b5cff
onur1 <onur1@onurgunduz.com>
2022-05-01 02:14:13 +0200

    Style to object null check

--- runtime/lib/style.js
+++ runtime/lib/style.js
@@ -4,8 +4,10 @@ const styleToObject = require('style-to-object')
 
 const keyPattern = /-(\w|$)/g
 
-module.exports = val =>
-  Object.entries(styleToObject(val)).reduce(
-    (acc, [k, v]) => ({ ...acc, ...{ [k.replace(keyPattern, upperCaseSecond)]: v } }),
-    {}
-  )
+module.exports = val => {
+  const o = styleToObject(val)
+  if (o === null) {
+    return {}
+  }
+  return Object.entries(o).reduce((acc, [k, v]) => ({ ...acc, ...{ [k.replace(keyPattern, upperCaseSecond)]: v } }), {})
+}
