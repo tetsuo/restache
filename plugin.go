e927dcc3657b9eebfaa979bd7140157dd8c1e817
tetsuo <ogunduz@gmail.com>
2025-05-04 00:21:05 +0200

    Remove filename checks

--- plugin.go
+++ plugin.go
@@ -2,13 +2,10 @@ package restache
 
 import (
 	"bytes"
-	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/evanw/esbuild/pkg/api"
-	"golang.org/x/net/html/atom"
 )
 
 func Plugin() api.Plugin {
@@ -32,21 +29,14 @@ func pluginBuildResolveHandler(args api.OnResolveArgs) (api.OnResolveResult, err
 }
 
 func pluginBuildLoadHandler(args api.OnLoadArgs) (api.OnLoadResult, error) {
-	e, err := newFileParser(args.Path)
+	root, err := parseFile(args.Path)
 	if err != nil {
 		return api.OnLoadResult{}, err
 	}
-
-	if err := e.parse(); err != nil {
-		return api.OnLoadResult{}, err
-	}
-
 	var buf bytes.Buffer
-
-	if _, err := Render(&buf, e.doc); err != nil {
+	if _, err := Render(&buf, root); err != nil {
 		return api.OnLoadResult{}, err
 	}
-
 	contents := buf.String()
 	return api.OnLoadResult{
 		Contents: &contents,
@@ -54,103 +44,16 @@ func pluginBuildLoadHandler(args api.OnLoadArgs) (api.OnLoadResult, error) {
 	}, nil
 }
 
-// fileParser parses a template with dependencies; it implements toposort.Vertex.
-type fileParser struct {
-	path string // full path (e.g. /foo/Bar.stache)
-	ext  string // extension (e.g. .stache)
-	stem string // filename without extension (e.g. Bar)
-	tag  string // lowercase stem (e.g. bar)
-	doc  *Node  // the root component node
-}
-
-func newFileParser(absPath string) (*fileParser, error) {
-	path := filepath.Base(absPath)
-	z := &fileParser{ext: filepath.Ext(path), path: absPath}
-	if z.ext != "" {
-		z.stem = path[:len(path)-len(z.ext)]
-	} else {
-		z.stem = path
-	}
-	if len(z.stem) == 0 {
-		// This also catches when path = "."
-		return nil, fmt.Errorf("input filename %q is not valid", path)
-	}
-	a := atom.Lookup([]byte(z.stem))
-	if a != 0 {
-		if _, ok := commonElements[a]; ok {
-			return nil, fmt.Errorf("invalid filename %q: <%s> conflicts with standard element", path, z.stem)
-		}
-	}
-	var hasUpper bool
-	hasUpper, err := isTagNameValidAndUpper(z.stem)
+func parseFile(path string) (*Node, error) {
+	f, err := os.Open(path)
 	if err != nil {
-		// This also catches ".."
-		return nil, fmt.Errorf("input filename %q %v", path, err)
-	}
-	if hasUpper {
-		z.tag = strings.ToLower(z.stem)
-	} else {
-		z.tag = z.stem
-	}
-	return z, nil
-}
-
-func (s *fileParser) parse() error {
-	f, err := os.Open(s.path)
-	if err != nil {
-		return err
+		return nil, err
 	}
 	defer f.Close()
-
-	p := newParser(f)
-	if err := p.parse(); err != nil {
-		return err
+	node, err := Parse(f)
+	if err != nil {
+		return nil, err
 	}
-
-	s.doc = p.doc
-	s.doc.Data = s.tag
-	s.doc.Path = []PathComponent{{Key: s.stem}, {Key: s.ext}}
-
-	return nil
-}
-
-// isTagNameValidAndUpper checks if the provided name is valid as HTML tag and
-// returns true if it contains at least one upper case letter.
-func isTagNameValidAndUpper(name string) (bool, error) {
-	if len(name) == 0 {
-		return false, fmt.Errorf("name is empty")
-	}
-
-	first := name[0]
-	if !isLetter(first) {
-		return false, fmt.Errorf("must start with an ASCII letter")
-	}
-
-	hasUpper := isUpper(first)
-	for i := 1; i < len(name); i++ {
-		c := name[i]
-		if isUpper(c) {
-			hasUpper = true
-		} else if !isLetter(c) && !isDigit(c) && c != '-' {
-			return false, fmt.Errorf("contains invalid character %q", c)
-		}
-	}
-
-	return hasUpper, nil
-}
-
-func isLetter(c byte) bool {
-	return isUpper(c) || isLower(c)
-}
-
-func isUpper(c byte) bool {
-	return c >= 'A' && c <= 'Z'
-}
-
-func isLower(c byte) bool {
-	return (c >= 'a' && c <= 'z')
-}
-
-func isDigit(c byte) bool {
-	return c >= '0' && c <= '9'
+	node.Data = "root"
+	return node, nil
 }
